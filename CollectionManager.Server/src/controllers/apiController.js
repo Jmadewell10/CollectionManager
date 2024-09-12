1e73c914b15aa50355f6b1d2213774598dad58cc
Jonathan Madewell <jonmadewell10@gmail.com>
2024-09-12 13:39:20 -0500

    removed node server

--- CollectionManager.Server/src/controllers/apiController.js
+++
@@ -1,8 +0,0 @@
-const getHello = (req, res) => {
-    res.json({ message: 'Hello from the API!' });
-  };
-  
-  module.exports = {
-    getHello,
-  };
-  
--- CollectionManager.Server/src/routes/apiRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getHello } = require('../controllers/apiController');
-
-// Define your API routes
-router.get('/hello', getHello);
-
-module.exports = router;
