9fde710f5b2fd784f00901d4daa225931b638d37
Jonathan Madewell <jonmadewell10@gmail.com>
2026-03-23 17:34:11 -0500

    remade ui pieces

--- CollectionManager.Client/CollectionManager/src/app/logged-out-home/logged-out-home.component.ts
+++ CollectionManager.Client/CollectionManager/src/app/logged-out-home/logged-out-home.component.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core';
-import { MatButton } from '@angular/material/button';
 import { MatDialog } from '@angular/material/dialog';
-import { Router } from '@angular/router';
 import { LoginComponent } from '../login/login.component';
 import { LoginService } from '../shared/services/login.service';
 import { AccountService } from '../shared/services/account.service';
@@ -13,50 +11,51 @@ import { MatSnackBar } from '@angular/material/snack-bar';
   templateUrl: './logged-out-home.component.html',
   styleUrl: './logged-out-home.component.scss'
 })
-
 export class LoggedOutHomeComponent {
-  
-  constructor(private dialog: MatDialog,
-     protected loginService: LoginService,
-     protected accountService: AccountService,
-     private snackBar: MatSnackBar
+
+  constructor(
+    private dialog: MatDialog,
+    protected loginService: LoginService,
+    protected accountService: AccountService,
+    private snackBar: MatSnackBar
   ) {}
 
-  login(){
-    let dialogRef = this.dialog.open(LoginComponent, {
-      width: '30%',
-      height: '60%'
-    })
-
-    dialogRef.afterClosed().subscribe((result) => {
-      if(result){
-        if(!result.email){
-          this.loginService.login(result.userName, result.password)
-          .subscribe((loginResuult) => {
-            if(loginResuult){
-              localStorage.setItem('token', loginResuult);
-              this.accountService.loginUser();
-            }
-            
-          });
-        }
-        if(result.email){
-          let request = {
-            login: {
-              userName: result.userName,
-              password: result.password
-            },
-            firstName: result.firstName,
-            lastName: result.lastName,
-            email: result.email
-          }
-          this.accountService.addAccount(request).subscribe((newAccountResponse) => {
-            this.snackBar.open(ACCOUNT_CREATED_MESSAGE);
-          });
-        }
-      }
+  login() {
+    const dialogRef = this.dialog.open(LoginComponent, {
+      width: '400px',
+      autoFocus: true,
+      panelClass: 'dark-dialog'
     });
 
-  }
+    dialogRef.afterClosed().subscribe((result) => {
+      if (!result) return;
 
+      if (!result.email) {
+        this.loginService.login(result.userName, result.password)
+          .subscribe((loginResult) => {
+            if (loginResult) {
+              localStorage.setItem('token', loginResult);
+              this.accountService.loginUser();
+            }
+          });
+      } else {
+        const request = {
+          login: {
+            userName: result.userName,
+            password: result.password
+          },
+          firstName: result.firstName,
+          lastName: result.lastName,
+          email: result.email
+        };
+
+        this.accountService.addAccount(request).subscribe(() => {
+          this.snackBar.open(ACCOUNT_CREATED_MESSAGE, 'Dismiss', {
+            duration: 4000,
+            panelClass: 'dark-snackbar'
+          });
+        });
+      }
+    });
+  }
 }
