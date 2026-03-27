5eb355d16ffd09ac70075b37cee0df706cda77a6
Jonathan Madewell <jonmadewell10@gmail.com>
2026-03-27 13:15:30 -0500

    wip: Added collections view and button to add new collection

--- CollectionManager.Client/CollectionManager/src/app/collections/collections.component.ts
+++ CollectionManager.Client/CollectionManager/src/app/collections/collections.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { Collection } from '../shared/models/collection-model';
 
 @Component({
   selector: 'app-collections',
@@ -6,5 +8,21 @@ import { Component } from '@angular/core';
   styleUrl: './collections.component.scss'
 })
 export class CollectionsComponent {
+
+  collections: Collection[] = [];
+
+  constructor(private dialog: MatDialog) { }
+
+  ngOnInit(): void {
+    // wire up your collections service here
+  }
+
+  openAddCollection(): void {
+    // open your add collection dialog here
+  }
+
+  openCollection(collection: Collection): void {
+    // navigate into the collection here
+  }
 
 }
---
+++ CollectionManager.Client/CollectionManager/src/app/shared/models/card-model.ts
@@ -0,0 +1,5 @@
+export interface Card {
+    cardId: string;
+    scryfallId?: string;
+    collectionId?: string;
+}
